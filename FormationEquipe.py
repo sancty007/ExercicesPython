8e61a05161981862f33ce79e1b98da7b17b7d5cd
sancty <sanctifieryawmensah55@gmail.com>
2023-10-12 13:15:04 +0100

    mise en place de deux listes de jouer , grace à une liste mélangé

---
+++ FormationEquipe.py
@@ -0,0 +1,69 @@
+import random 
+
+# studen list 
+names = ["Alice", "Bob", "Charlie", "David", "Eve", "Frank", "Grace", "Hannah"]
+
+"""
+    randomList : perme de melanger une liste et 
+    retourne une nouvelle liste 
+"""
+def randomList(myList) :
+    try :        
+        return random.sample(myList,len(myList)) # return new list
+    except :
+        print("Un probleme dans le code..! Veuillez donner une liste en paramètre")
+
+"""
+halfList : retourne -> la longueur de la moitier de la liste
+"""
+def halfList(newList)-> int:
+    return len(newList)//2#cut length of the list in half
+ 
+"""
+printeTwoLists :permet retourner -> deux listes grace au tuples
+"""
+def printeTwoLists(newList):
+
+    half = halfList(newList)
+    firstList =[i for i in newList[:half]]
+    secondtList =[i for i in newList[half:]]
+    return (firstList , secondtList) # -> return tuple 
+
+    
+newList =randomList(names)
+firstList,secondtList =printeTwoLists(newList)
+
+
+print(firstList)
+print("----------------------------------")
+print(secondtList)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+""" 
+    for element, element2 in zip(nouvelleListe ,noms) :
+        print(f"{element}{element2}") 
+"""
+
+
+
+
+
