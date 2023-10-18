103e343587b5b32a4e1948c7fbca4d257c8666e0
sancty <sanctifieryawmensah55@gmail.com>
2023-10-18 18:53:57 +0100

    ajout fonction avec slicing , inverse chaine

--- InverserUneChaine.py
+++ InverserUneChaine.py
@@ -1,18 +1,42 @@
 
-maChaine = "mon paragraphe"
 
+
+# parcour de la chaine à l'envers 
 def inverser_chaine_reverse(maChaine) :
     chaine_reverse =""
 
     for char in reversed(maChaine) :
-        chaine_reverse =chaine_reverse + char
+        chaine_reverse =chaine_reverse + char 
 
     return chaine_reverse
 
+# la chaine à l'endroit
 
-chaine = inverser_chaine_reverse(maChaine)
+def inverser_chaine_Endroit(maChaine):
+    chaine_inverse = ""
+    for caractere in maChaine:
+        chaine_inverse = caractere + chaine_inverse
 
-print(chaine)
+    return chaine_inverse
+
+
+# inverse avec slicing 
+def inverse_slicing(maChaine) :
+    return maChaine[::-1] # un pas de -1 
+
+
+""" chaine =inverser_chaine_reverse(maChaine)
+
+print(chaine)  """
+
+maChaine = "mon paragraphe"
+
+
+print(inverser_chaine_Endroit(maChaine))
+
+
+
+
 
 
 
